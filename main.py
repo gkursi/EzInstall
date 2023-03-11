0edd2adcd84ea149c62ceb40920275edd468d8ab
gkursi <itsbookx@gmail.com>
2023-03-11 22:50:15 +0300

    added more input

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ print(Fore.CYAN+'''
 ███████╗███████╗██║██║░╚███║██████╔╝░░░██║░░░██║░░██║███████╗███████╗
 ╚══════╝╚══════╝╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝
 '''+Fore.BLUE+"                                                            Generator")
+print(Fore.GREEN+"If you need help, refer to the docs at github.com/gkursi/ezinstall/ ")
 
-# input
-filename = input(Fore.WHITE+'Enter your desired file name: ')
-filehost = input("Enter the zip file URL: ");
+# input (for the gen file)
+g_name = input(Fore.WHITE+'Enter your desired file name: ')
+g_dir = input("Enter the full output path for the generated file: ")
 
+# input (for the installation)
+o_filehost = input("Enter the zip file URL: ");
+o_
 
 # magic
 
